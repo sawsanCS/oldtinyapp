f7d7024a5c4c6664255110d49e1ef835ba0c7e69
saoussen <sawsan_sli3i@live.fr>
2020-11-10 10:36:03 +0000

    new changes

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
+//app.set("views", "path/to/views");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +12,16 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+  app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL]
+   
+    };
+    res.render("urls_show", templateVars); 
+  });
+  
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
