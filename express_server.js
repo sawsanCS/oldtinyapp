f3636063d0c25bbf3ba75c6eefff58402dd12ba1
saoussen <sawsan_sli3i@live.fr>
2020-11-10 17:48:09 +0000

    randomString function created

--- express_server.js
+++ express_server.js
@@ -3,24 +3,39 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 //app.set("views", "path/to/views");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+console.log(generateRandomString());
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
   app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { 
       shortURL: req.params.shortURL, 
       longURL: urlDatabase[req.params.shortURL]
-   
     };
     res.render("urls_show", templateVars); 
   });
+ 
+  
   
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
