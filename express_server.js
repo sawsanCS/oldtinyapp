444f6bc7379e4f01808ae868a680068b51d3952b
saoussen <sawsan_sli3i@live.fr>
2020-11-10 09:39:00 +0000

    ejs added as view engine

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.set('view engine', 'ejs');
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,9 +9,21 @@ const urlDatabase = {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+ 
 
 
 app.listen(PORT, () => {
