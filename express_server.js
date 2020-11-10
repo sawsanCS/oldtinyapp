a5dcf2bb570eeff8b13bd0b19b751358e0440eab
saoussen <sawsan_sli3i@live.fr>
2020-11-10 09:58:44 +0000

    changes made to render urls

--- express_server.js
+++ express_server.js
@@ -6,11 +6,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 app.get("/", (req, res) => {
   res.send("Hello!");
